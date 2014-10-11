16ec3957e759829d0c9b06a679717047afc824b7
jramapuram <jason.ramapuram@gmail.com>
2014-10-11 02:11:31 -0700

    add parsing logic

--- DatabaseLoader.py
+++ DatabaseLoader.py
@@ -3,6 +3,7 @@ __author__ = 'jason'
 import os.path
 import bcrypt
 import leveldb
+import sys
 from simplecrypt import encrypt, decrypt
 
 
@@ -22,43 +23,53 @@ class DatabaseLoader:
             print 'Error, invalid DB pwd!'
         return db_kv_set
 
-    #TODO : implement a CLI parser to save to an xml file & add optional number of email logins
-    # def parse_cli(self):
-    #     num_bills = int(raw_input('Enter the number of bills:'))
-    #     self.data_map['num_bills'] = num_bills
-    #
-    #     # get all the bill ratios
-    #     for j in xrange(num_bills):
-    #         bill_name = raw_input("Enter bill #%d name :" % j)
-    #         for user in users:
-    #             current_pwd = raw_input("Enter bill ratio for user %s :" % user)
-    #             self.data_map['user2jr_%s' % current_user] = current_pwd
-
     def load(self):
         data_map = {}
         if os.path.isdir(self.db_file_location):
             data_map = self.load_database(self.db_file_location, self.db_pwd)
         else:
-            print 'Setting up rentbot for first run...'
-            num_users = int(raw_input('Enter the number of users:'))
+            # Get our users and toss them into a set to cleanup dupes
+            available_users = []
+            is_email_reqd = False
+            for bill in self.bill_list:
+                if '@' in bill[bill.root].data:  # Check to see if we require emails
+                    is_email_reqd = True
+                for node in bill.children(bill.root):
+                    available_users.append(node.identifier)
+            available_users = set(available_users)
 
-            # get all users and setup their login info into db
-            for i in xrange(num_users):  # TODO: change to stop input using null statement
-                current_user = raw_input("Enter user #%d's email :" % i)
-                imap_server = raw_input("Enter imap server for %s :" % current_user)
-                current_pwd = raw_input("Enter password for user %s :" % current_user)
-                data_map['%s#%s' % (current_user, imap_server)] = current_pwd
+            if is_email_reqd:
+                print 'Setting up rentbot for first run...'
+                for user in available_users:
+                    if raw_input("Would you like to use %s\'s email to pull bills?" % user).lower().strip() == "yes":
+                        current_user = raw_input("Enter %s\'s email address :" % user)
+                        imap_server = raw_input("Enter %s\'s imap server :" % user)
+                        smtp_server = ""
+                        if self.dest.lower().strip() == "email":
+                            smtp_server = raw_input("Enter %s\'s smtp server:" % user)
+                        current_pwd = raw_input("Enter %s\'s password :" % user)
+                        data_map['%s#%s' % (current_user, imap_server)] = current_pwd  # XXX
+                        data_map['%s|%s' % (current_user, smtp_server)] = current_pwd  # XXX
+                    else:
+                        print "I\'m assuming that was a no.."
 
-            # save all the values we now have
-            self.db_pwd = raw_input("Enter a password to encrypt the database: ")
-            ldb = leveldb.LevelDB(self.db_file_location)
-            ldb.Put('password', bcrypt.hashpw(self.db_pwd, bcrypt.gensalt()))
-            for k, v in data_map.iteritems():
-                ldb.Put(k, encrypt(self.db_pwd, v))
+            # Some minimal checking
+            if is_email_reqd and len(data_map) > 0:
+                # save all the values we now have
+                self.db_pwd = raw_input("Enter a master password to encrypt the database: ")
+                ldb = leveldb.LevelDB(self.db_file_location)
+                ldb.Put('password', bcrypt.hashpw(self.db_pwd, bcrypt.gensalt()))
+                for k, v in data_map.iteritems():
+                    ldb.Put(k, encrypt(self.db_pwd, v))
+            elif is_email_reqd:
+                print "XML file states that some data is to be parsed from email."
+                print "Please re-run the program & enter at LEAST 1 email (or) don't use an email topology"
+                sys.exit(-1)  # XXX
 
         return data_map
 
-    def __init__(self, bill_list, db_file_location, db_pwd):
+    def __init__(self, bill_list, db_file_location, db_pwd, dest):
         self.db_file_location = db_file_location
         self.bill_list = bill_list
         self.db_pwd = db_pwd
+        self.dest = dest
--- Rentbot.py
+++ Rentbot.py
@@ -2,9 +2,11 @@ __author__ = 'jason'
 
 import imaplib
 import re
+import sys
 import time
 from treelib import Tree
 from os import path
+from collections import defaultdict
 import xml.etree.ElementTree as ET
 from DatabaseLoader import DatabaseLoader
 
@@ -14,6 +16,7 @@ tree_location = "./rent.xml"
 
 #returns a logged in imap object with the inbox folder selected
 def login_user(user_name, imap_server, pwd):
+    print imap_server, user_name, pwd
     mail = imaplib.IMAP4_SSL(imap_server)
     mail.login(user_name, pwd)
     mail.select("inbox")
@@ -22,6 +25,7 @@ def login_user(user_name, imap_server, pwd):
 def parse_xml(path):
     tree = ET.parse(path)
     bill_list = []
+    destination = "console"
 
     # eg:       (cox, "Do_Not_Reply_Prod@cox.com")                  (rent, 2000)
     #          /                                 \            &    /            \
@@ -29,7 +33,6 @@ def parse_xml(path):
     for bill in tree.findall("bill"):
         billname = bill.get("name")
         bill_tree = Tree()
-
         bill_value = bill.get("fixed")
         if bill_value is None:
             bill_value = bill.get("from_email")
@@ -41,12 +44,13 @@ def parse_xml(path):
             bill_tree.create_node(tag=username, identifier=username, parent=billname, data=ratio)
         bill_list.append(bill_tree)
 
-    for bt in bill_list:
-        print bt.to_json(with_data=True)
+    # Get the location to dump our results
+    for d in tree.findall("output"):
+        destination = d.get("destination")
 
-    return bill_list
+    return (bill_list, destination)
 
-def parse_amount(imap_object, from_user, regex):
+def parse_amount_email(imap_object, from_user, regex):
     date_str = time.strftime("01-%b-%Y")
     result, data = imap_object.uid('search', None, '(FROM "%s" SENTSINCE "%s")' % (from_user, date_str))
 
@@ -59,29 +63,66 @@ def parse_amount(imap_object, from_user, regex):
 
 def parse_emails(user_db, bill_list):
     for k, v in user_db.items():
-        user_data = k.split('#')
-        assert len(user_data) == 2  # eg:'name#email', TODO: have option to not require everyone's email
-        current_user = login_user(user_data[0], user_data[1], v)
-        for bill in bill_list:  # per bill
-            for node in bill.subtree(bill.root).expand_tree(mode=Tree.DEPTH):
-                if '@' in bill[node].data:
-                    print parse_amount(current_user, bill[node].data, "\$\s*\d+\.\d*")
+        if '#' in k:  # XXX
+            user_data = k.split('#')
+            assert len(user_data) == 2  # eg:'name#email'
+            current_user = login_user(user_data[0], user_data[1], v)
+            for bill in bill_list:  # per bill
+                for node in bill.subtree(bill.root).expand_tree(mode=Tree.DEPTH):
+                    if '@' in bill[node].data:
+                        value = parse_amount_email(current_user, bill[node].data, "\$\s*\d+\.\d*")
+                        print "value: ",value
+                        bill[node].data = value
 
-def get_db(bill_list):
+def get_db(bill_list, dest):
     db_pwd = None
     if path.isdir(db_file_location):
         db_pwd = raw_input("Please enter db password to continue: ")
-    dbl = DatabaseLoader(bill_list, db_file_location, db_pwd)
+    dbl = DatabaseLoader(bill_list, db_file_location, db_pwd, dest)
     user_db = dbl.load()
     return user_db
 
+def parse_payments(bill_list):
+    for bill in bill_list:
+        # at this point we assume that we have replaced emails with values
+        if '@' in bill[bill.root].data:
+            print "Error : Could not fill in data for %s bill from email %s" % (bill[bill.root].identifier, bill[bill.root].data)
+            sys.exit(-3) # XXX
+
+        amount = float(bill[bill.root].data)
+
+        #multiply out our rations
+        for node in bill.children(bill.root):
+            node.data = str(float(node.data) * amount)
 
 if __name__ == '__main__':
-    #parse xml file, TODO: use this in the data parsing
-    bill_list = parse_xml(tree_location)
+    print'                       __ ___.           __'
+    print'_______   ____   _____/  |\_ |__   _____/  |_'
+    print'\_  __ \_/ __ \ /    \   __\ __ \ /  _ \   __\\'
+    print' |  | \/\  ___/|   |  \  | | \_\ (  <_> )  |  '
+    print' |__|    \___  >___|  /__| |___  /\____/|__|  '
+    print'             \/     \/         \/             '
+    print'version 0.01'
+    print' '
+
+    #parse xml file
+    (bill_list, dest) = parse_xml(tree_location)
 
     #get a handle to our database
-    user_db = get_db(bill_list)
+    user_db = get_db(bill_list, dest)
 
-    # now create imap objects & query it
-    parse_emails(user_db, bill_list)
+    # query email for the required amounts
+    for key in user_db.keys():
+        if '#' in key:  # XXX
+            parse_emails(user_db, bill_list)
+            break
+
+    parse_payments(bill_list)
+    bill_totals = defaultdict(lambda: 0)
+    for bill in bill_list:
+        for node in bill.children(bill.root):
+            print "{0:20} owes $ {1:7} for {2:20}".format(node.identifier, node.data, bill.root)
+            bill_totals[node.identifier] += float(node.data)
+
+    print "\n\nGrand Totals:"
+    print bill_totals
